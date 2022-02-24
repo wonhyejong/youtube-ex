efd3774cbe72bc97a9269670f250d368b829045e
wonhjteacher <wonhjteacher@gmail.com>
2022-02-24 12:29:20 +0900

    feature add videolist

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
 import './App.css';
-
+import {useState} from 'react';
+import {useEffect} from 'react';
+import VideoList from './components/videolist/VideoList';
 function App() {
+  const [videoItems,setVideoItems] = useState([]);
+  useEffect( () => {
+    const requestOptions = {
+        method: 'GET',
+        redirect: 'follow',
+      };
+
+      fetch(
+        'https://youtube.googleapis.com/youtube/v3/videos?part=snippet&chart=mostPopular&maxResults=30&key=AIzaSyDAB5uGTFPefYJGfYUjV9PxFTjZqFttRx0',
+        requestOptions
+      )
+    .then(response => response.json())
+    .then(result => setVideoItems(result.items))
+    .catch(error => console.log('error', error));
+    },[]);
   return (
     <div className="App">
-      
+      <VideoList videoItems={videoItems} />
     </div>
   );
 }
---
+++ src/components/video/Video.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+function Video(props){
+    return(
+        <li>
+            {props.videoItem.snippet.channelTitle}
+        </li>
+    )
+}
+export default Video;
